91903832c9cc28d01603be370aa9eb632a770ea2
Kevin Finley <kftwotwo@gmail.com>
2017-02-24 10:21:05 -0800

    make a tabe for jobs and fox link for jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,7 +16,10 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
     if @job.save
-      redirect_to jobs_path
+      respond_to do |format|
+        format.html { redirect_to jobs_path }
+        format.js
+      end
     else
       render :new
     end
