c1acd295b89e4a09c12b6571228953061565d728
Kevin Finley <kftwotwo@gmail.com>
2017-02-24 14:51:38 -0800

    allow a worker to claim a job with ajax

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -26,9 +26,12 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if current_worker
-      if @job.update(pending: true, worker_id: current_worker.id)
-        redirect_to worker_path(current_worker)
+      if @job.update(pending: false, worker_id: current_worker.id)
         flash[:notice] = "You've successfully claimed this job."
+        respond_to do |format|
+          format.html { worker_path(current_worker) }
+          format.js
+        end
       else
         render :show
         flash[:notice] = "Something went wrong!"
